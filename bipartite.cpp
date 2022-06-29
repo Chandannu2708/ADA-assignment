a37bc1df67ffa153f79d513fa6f0a71029c370bf
Chandannu2708 <108363344+Chandannu2708@users.noreply.github.com>
2022-06-29 09:40:10 +0530

    Update bipartite.cpp

--- bipartite.cpp
+++ bipartite.cpp
@@ -1,59 +1,47 @@
-#include<iostream>
-using namespace std;
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
 #define M 6
 #define N 6
-
-bool Check(int a[][N],int m[],bool c[], int u) 
-{
-  for(int i=0;i<N;i++)
-  {
-    if(a[u][i] && !c[i])
-    {
-      c[i]=true;
-      if(m[i]<0 || Check(a,m,c,m[i])) 
-      {
-        m[i]=u;
+bool bipartiteGraph[M][N];
+bool bipartiteMax(int u,bool visited[],int a[]){
+  for(int v=0;v<N;v++){
+    if(bipartiteGraph[u][v]&&!visited[v]){
+      visited[v]=true;
+      if(a[v]<0||bipartiteMax(a[v],visited, a)){
+        a[v]=u;
         return true;
       }
     }
   }
   return false;
 }
-
-int BMC(int a[][N])
-{
-    int match[N],result=0;
-    bool check[N]; 
-
+  int maxMatch(){
+    int a[N];
     for(int i=0;i<N;i++)
-    {
-      match[i]=-1;
-      check[i]=false;
+      a[i]=-1;
+    int Count=0;
+    for(int u=0;u<M;u++){
+      bool visited[N];
+      for(int i=0;i<N;i++)
+        visited[i]=false;
+      if(bipartiteMax(u,visited,a))
+        Count++;
     }
-
-    for(int i=0;i<M;i++) 
+return Count;
+  }
+  int main(){
+printf("Enter the matrix:");
+    int i,j;
+    int graph[M][N];
+    for(i=0;i<M;i++)
     {
-      if(Check(a,match,check,i))
-        result++;
+      for(j=0;j<N;j++)
+      {
+        scanf("%d",&graph[i][j]);
+        bipartiteGraph[i][j]=graph[i][j];
+      }
     }
-
-    return result;
-}
-
-int main()
-{
-    int a[M][N]; 
-    cout<<"Enter the adjacency matrix\n";
-
-    for(int i=0;i<M;i++)
-    {
-        for(int j=0;j<N;j++)
-        {
-            cin>>a[i][j];
-        }
-    }
-
-    cout<<BMC(a);
-    return 0;
-}
+    printf("The maximum cover possible is:%d",maxMatch());
+  }
+                  
